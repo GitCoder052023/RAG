abed3b7147de67f93196d1bd983d2f4b0f035617
GitCoder0502023 <K.alam93899@gmail.com>
2026-03-18 23:37:52 +0530

    chore(server): remove Que System

--- server/index.js
+++ server/index.js
@@ -1,19 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import multer from 'multer';
-import { Queue } from 'bullmq';
 import { OpenAIEmbeddings } from '@langchain/openai';
 import { QdrantVectorStore } from '@langchain/qdrant';
+import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
 import OpenAI from 'openai';
 
 const client = new OpenAI({
   apiKey: '',
-});
-const queue = new Queue('file-upload-queue', {
-  connection: {
-    host: 'localhost',
-    port: '6379',
-  },
 });
 
 const storage = multer.diskStorage({
@@ -36,15 +30,29 @@ app.get('/', (req, res) => {
 });
 
 app.post('/upload/pdf', upload.single('pdf'), async (req, res) => {
-  await queue.add(
-    'file-ready',
-    JSON.stringify({
-      filename: req.file.originalname,
-      destination: req.file.destination,
-      path: req.file.path,
-    })
+  const filePath = req.file.path;
+
+  // Load the PDF
+  const loader = new PDFLoader(filePath);
+  const docs = await loader.load();
+
+  const embeddings = new OpenAIEmbeddings({
+    model: 'text-embedding-3-small',
+    apiKey: '',
+  });
+
+  const vectorStore = await QdrantVectorStore.fromExistingCollection(
+    embeddings,
+    {
+      url: 'http://localhost:6333',
+      collectionName: 'langchainjs-testing',
+    }
   );
-  return res.json({ message: 'uploaded' });
+
+  await vectorStore.addDocuments(docs);
+  console.log(`All docs are added to vector store`);
+
+  return res.json({ message: 'uploaded and processed' });
 });
 
 app.get('/chat', async (req, res) => {
